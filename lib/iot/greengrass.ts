f4657e23289453d4a5ffcb76551984aa6070c968
Ahmed Elsenousi <aelsenou@amazon.com>
2022-10-14 11:11:06 +0100

    fix logmanager

--- lib/iot/greengrass.ts
+++ lib/iot/greengrass.ts
@@ -69,118 +69,119 @@ export class IotCore extends Construct {
 
   createComponents(watertankName: string, virtual = true) {
     const prod = virtual ? 0 : 1;
-    const components = [this.createComponent('amper_meter', {
-      amps_activity_threshold: '610',
-      avg_size: '3',
-      frequency: '1',
-      ipc_in_prefix: 'raw/',
-      ipc_out_prefix: 'state/',
-      sensors: ['amps_meter_1', 'amps_meter_2'],
-    }),
-    this.createComponent('compute_status', {
-      amps_activity_threshold: '610',
-      capacity: '5',
-      computes: ['volume_level', 'pump_1_active', 'pump_2_active', 'leak'],
-      flow_activity_threshold: '5',
-      frequency: '1',
-      ipc_in_prefix: 'state/',
-      ipc_out_prefix: 'compute/',
-      leak_delta: '8',
-      ohms_max_level: '520',
-      ohms_min_level: '571',
-      sensors: ['flow_meter_1', 'flow_meter_2', 'amps_meter_1', 'amps_meter_2', 'ohms_meter'],
-      state_switch_threshold: '1',
-    }),
-    this.createComponent('data_generator', {
-      controls: ['leak', 'pump_1', 'pump_2'],
-      frequency: '1',
-      mqtts_cmd_prefix: 'WaterTank_13/simulation/cmd/',
-      sensors: ['ohms', 'amps_1', 'amps_2', 'flow_1', 'flow_2'],
-      serial_simulation_ipc_topic: 'raw/serial',
-      temperature_simulation_ipc_topic: 'raw/temperature',
-    }),
-    this.createComponent('flow_meter', {
-      avg_size: '3',
-      flow_activity_threshold: '615',
-      frequency: '1',
-      ipc_in_prefix: 'raw/',
-      ipc_out_prefix: 'state/',
-      sensors: ['flow_meter_1', 'flow_meter_2'],
-    }),
-    this.createComponent('lcd_manager', {
-      frequency: '1',
-      ipc_flow_meter: 'state/flow_meter_2',
-      ipc_temperature: 'state/temperature',
-      ipc_volume_level: 'compute/volume_level',
-    }),
-    this.createComponent('led_manager', {
-      frequency: '1',
-      ipc_in_prefix: 'compute/',
-      ipc_out_prefix: 'command/',
-      sensors: ['pump_1_active', 'pump_2_active', 'pump_1_overright', 'pump_2_overright', 'leak_overright', 'leak'],
-    }),
-    this.createComponent('ohms_meter', {
-      avg_size: '3',
-      frequency: '1',
-      ipc_in_prefix: 'raw/',
-      ipc_out_prefix: 'state/',
-      sensors: ['ohms_meter'],
-    }),
-    this.createComponent('remote_control', {
-      actuators: ['pump_1', 'pump_2', 'leak', 'demo'],
-      default_state_leak: 0,
-      default_state_pump_1: 1,
-      default_state_pump_2: 1,
-      demo_drain_time: 28,
-      demo_leak_time: 15,
-      demo_recovery_time: 2,
-      demo_regular_operation_time: 10,
-      frequency: '1',
-      ipc_out_prefix: 'compute/',
-      leak_pin: '23',
-      mqtts_in_prefix: watertankName + '/',
-      relay_pump_1_pin: '17',
-      relay_pump_2_pin: '12',
-    }),
-    this.createComponent('serial_split', {
-      frequency: '1',
-      ipc_led_control: 'command/led_manager',
-      ipc_out_prefix: 'raw/',
-      ipc_serial_simulation: 'raw/serial',
-      prod,
-      sensors: ['ohms_meter', 'amps_meter_1', 'amps_meter_2', 'flow_meter_1', 'flow_meter_2'],
-      serial_port: '/dev/ttyS0',
-      serial_speed: '9600',
-    }),
-    this.createComponent('temperature', {
-      avg_size: '3',
-      frequency: '1',
-      ipc_in_prefix: 'raw/',
-      ipc_out_prefix: 'state/',
-      prod,
-      sensors: ['temperature'],
-      temperature_base_dir: '/sys/bus/w1/devices/28*',
-    }),
-    this.createComponent('timestream_sync', {
-      alarm_asset_type: 'Alarm',
-      alarm_key_id: watertankName,
-      alarms: ['leak'],
-      computes: ['volume_level', 'pump_1_active', 'pump_2_active', 'leak'],
-      default_reporting_state: 1,
-      frequency: '1',
-      ipc_compute_prefix: 'compute/',
-      ipc_in_prefix: 'state/',
-      mqtts_comm_control_topic: watertankName + '/control/send_data',
-      mqtts_out_alarm: watertankName + '/state/alarm',
-      mqtts_out_status: watertankName + '/state/status',
-      mqtts_out_telemetry: watertankName + '/state/telemetry',
-      sensors: ['ohms_meter', 'amps_meter_1', 'amps_meter_2', 'flow_meter_1', 'flow_meter_2', 'temperature'],
-      status: ['leak'],
-      status_asset_type: 'Status',
-      status_key_id: watertankName,
-      telemetry_asset_id: watertankName,
-      telemetry_asset_type: 'Telemetry',
-    }),
+    const components = [
+      this.createComponent('amper_meter', {
+        amps_activity_threshold: '610',
+        avg_size: '3',
+        frequency: '1',
+        ipc_in_prefix: 'raw/',
+        ipc_out_prefix: 'state/',
+        sensors: ['amps_meter_1', 'amps_meter_2'],
+      }),
+      this.createComponent('compute_status', {
+        amps_activity_threshold: '610',
+        capacity: '5',
+        computes: ['volume_level', 'pump_1_active', 'pump_2_active', 'leak'],
+        flow_activity_threshold: '5',
+        frequency: '1',
+        ipc_in_prefix: 'state/',
+        ipc_out_prefix: 'compute/',
+        leak_delta: '8',
+        ohms_max_level: '520',
+        ohms_min_level: '571',
+        sensors: ['flow_meter_1', 'flow_meter_2', 'amps_meter_1', 'amps_meter_2', 'ohms_meter'],
+        state_switch_threshold: '1',
+      }),
+      this.createComponent('data_generator', {
+        controls: ['leak', 'pump_1', 'pump_2'],
+        frequency: '1',
+        mqtts_cmd_prefix: 'WaterTank_13/simulation/cmd/',
+        sensors: ['ohms', 'amps_1', 'amps_2', 'flow_1', 'flow_2'],
+        serial_simulation_ipc_topic: 'raw/serial',
+        temperature_simulation_ipc_topic: 'raw/temperature',
+      }),
+      this.createComponent('flow_meter', {
+        avg_size: '3',
+        flow_activity_threshold: '615',
+        frequency: '1',
+        ipc_in_prefix: 'raw/',
+        ipc_out_prefix: 'state/',
+        sensors: ['flow_meter_1', 'flow_meter_2'],
+      }),
+      this.createComponent('lcd_manager', {
+        frequency: '1',
+        ipc_flow_meter: 'state/flow_meter_2',
+        ipc_temperature: 'state/temperature',
+        ipc_volume_level: 'compute/volume_level',
+      }),
+      this.createComponent('led_manager', {
+        frequency: '1',
+        ipc_in_prefix: 'compute/',
+        ipc_out_prefix: 'command/',
+        sensors: ['pump_1_active', 'pump_2_active', 'pump_1_overright', 'pump_2_overright', 'leak_overright', 'leak'],
+      }),
+      this.createComponent('ohms_meter', {
+        avg_size: '3',
+        frequency: '1',
+        ipc_in_prefix: 'raw/',
+        ipc_out_prefix: 'state/',
+        sensors: ['ohms_meter'],
+      }),
+      this.createComponent('remote_control', {
+        actuators: ['pump_1', 'pump_2', 'leak', 'demo'],
+        default_state_leak: 0,
+        default_state_pump_1: 1,
+        default_state_pump_2: 1,
+        demo_drain_time: 28,
+        demo_leak_time: 15,
+        demo_recovery_time: 2,
+        demo_regular_operation_time: 10,
+        frequency: '1',
+        ipc_out_prefix: 'compute/',
+        leak_pin: '23',
+        mqtts_in_prefix: watertankName + '/',
+        relay_pump_1_pin: '17',
+        relay_pump_2_pin: '12',
+      }),
+      this.createComponent('serial_split', {
+        frequency: '1',
+        ipc_led_control: 'command/led_manager',
+        ipc_out_prefix: 'raw/',
+        ipc_serial_simulation: 'raw/serial',
+        prod,
+        sensors: ['ohms_meter', 'amps_meter_1', 'amps_meter_2', 'flow_meter_1', 'flow_meter_2'],
+        serial_port: '/dev/ttyS0',
+        serial_speed: '9600',
+      }),
+      this.createComponent('temperature', {
+        avg_size: '3',
+        frequency: '1',
+        ipc_in_prefix: 'raw/',
+        ipc_out_prefix: 'state/',
+        prod,
+        sensors: ['temperature'],
+        temperature_base_dir: '/sys/bus/w1/devices/28*',
+      }),
+      this.createComponent('timestream_sync', {
+        alarm_asset_type: 'Alarm',
+        alarm_key_id: watertankName,
+        alarms: ['leak'],
+        computes: ['volume_level', 'pump_1_active', 'pump_2_active', 'leak'],
+        default_reporting_state: 1,
+        frequency: '1',
+        ipc_compute_prefix: 'compute/',
+        ipc_in_prefix: 'state/',
+        mqtts_comm_control_topic: watertankName + '/control/send_data',
+        mqtts_out_alarm: watertankName + '/state/alarm',
+        mqtts_out_status: watertankName + '/state/status',
+        mqtts_out_telemetry: watertankName + '/state/telemetry',
+        sensors: ['ohms_meter', 'amps_meter_1', 'amps_meter_2', 'flow_meter_1', 'flow_meter_2', 'temperature'],
+        status: ['leak'],
+        status_asset_type: 'Status',
+        status_key_id: watertankName,
+        telemetry_asset_id: watertankName,
+        telemetry_asset_type: 'Telemetry',
+      }),
     ];
     for (let i = 1; i < components.length; i++) components[i].component.addDependsOn(components[i - 1].component);
     for (let i = 1; i < components.length; i++) components[i].versionCr.node.addDependency(components[i - 1].versionCr);
@@ -196,7 +197,7 @@ export class IotCore extends Construct {
           },
         },
       }),
-      {},
+      {}
     );
 
     const awsComponents = {
@@ -207,108 +208,43 @@ export class IotCore extends Construct {
         componentVersion: '2.7.0',
         configurationUpdate: {
           merge: JSON.stringify({
-            deploymentPollingFrequency: "60",
+            deploymentPollingFrequency: '60',
             logging: {
-              level: "INFO",
-              format: "TEXT",
-              outputType: "FILE",
-              fileSizeKB: "1024"
-            }
-          })
-        }
+              level: 'INFO',
+              format: 'TEXT',
+              outputType: 'FILE',
+              fileSizeKB: '1024',
+            },
+          }),
+        },
       },
       'aws.greengrass.LogManager': {
-        componentVersion: "2.2.6",
+        componentVersion: '2.2.6',
         configurationUpdate: {
           merge: JSON.stringify({
-            "logsUploaderConfiguration": {
-              "systemLogsConfiguration": {
-                "uploadToCloudWatch": "true",
-                "minimumLogLevel": "INFO",
-                "diskSpaceLimit": "10",
-                "diskSpaceLimitUnit": "MB",
-                "deleteLogFileAfterCloudUpload": "true"
+            logsUploaderConfiguration: {
+              systemLogsConfiguration: {
+                uploadToCloudWatch: 'true',
+                minimumLogLevel: 'INFO',
+                diskSpaceLimit: '10',
+                diskSpaceLimitUnit: 'MB',
+                deleteLogFileAfterCloudUpload: 'true',
               },
-              "componentLogsConfiguration": [
-                {
-                  "componentName": "aws.prototyping.digital_twin.amper_meter",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                },
-                {
-                  "componentName": "aws.prototyping.digital_twin.compute_status",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                },
-                {
-                  "componentName": "aws.prototyping.digital_twin.flow_meter",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                },
-                {
-                  "componentName": "aws.prototyping.digital_twin.lcd_manager",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                },
-                {
-                  "componentName": "aws.prototyping.digital_twin.led_manager",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                },
-                {
-                  "componentName": "aws.prototyping.digital_twin.ohms_meter",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                },
-                {
-                  "componentName": "aws.prototyping.digital_twin.remote_control",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                },
-                {
-                  "componentName": "aws.prototyping.digital_twin.serial_split",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                },
-                {
-                  "componentName": "aws.prototyping.digital_twin.temperature",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                },
-                {
-                  "componentName": "aws.prototyping.digital_twin.timestream_sync",
-                  "deleteLogFileAfterCloudUpload": "true",
-                  "diskSpaceLimit": "10",
-                  "diskSpaceLimitUnit": "MB",
-                  "minimumLogLevel": "INFO"
-                }
-              ]
+              componentLogsConfiguration: components.map(({ component }) => ({
+                componentName: component.attrComponentName,
+                deleteLogFileAfterCloudUpload: 'true',
+                diskSpaceLimit: '10',
+                diskSpaceLimitUnit: 'MB',
+                minimumLogLevel: 'INFO',
+              })),
             },
-            "periodicUploadIntervalSec": "60"
-          })
-        }
+            periodicUploadIntervalSec: '60',
+          }),
+        },
       },
       'aws.greengrass.TokenExchangeService': {
-        componentVersion: '2.0.3'
-      }
+        componentVersion: '2.0.3',
+      },
     };
 
     return {
